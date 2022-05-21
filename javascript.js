a77e0a98dd098fa398e22ee09ce56cfa3c65cdfa
Giorgio <tran.giorgio@gmail.com>
2022-05-20 23:41:26 -1000

    Attempted to implement the playing game encountered loop bug

--- javascript.js
+++ javascript.js
@@ -1,19 +1,7 @@
 const boxes = document.querySelectorAll('.box');
 
-boxes.forEach((box) => {
-    // add a click listener for each box
-    box.addEventListener('click', () => {
-        console.log(box.id);
-    });
-});
+boxes.forEach(box => box.addEventListener('click', playGame));
 
-//make box go back to normal when clicked
-// boxes.forEach((box) => {
-//     // add a click listener for each box
-//     box.addEventListener('click', function() {
-//         this.classList.toggle('half');
-//     });
-// });
 boxes.forEach(box => box.addEventListener('transitionend',removeTransition));
 
 boxes.forEach(box => box.addEventListener('click', decreaseSizeOnClick));
@@ -26,6 +14,59 @@ function removeTransition(e) {
     if (e.propertyName !== 'transform') return;
     e.target.classList.remove('small');
 }
+
+//Play a game 
+function playGame(e) {
+    let cpuPoints = 0;
+    let playerPoints = 0;
+    let selection = ["rock","paper","scissors"];
+    let choice = e.currentTarget.id;
+    
+    function computerPlay() {
+        return selection[Math.floor(Math.random() * 3)];
+    }
+
+    function playARound(computerSelection, playerSelection) {
+        //if tie, return 0
+        if (computerSelection === playerSelection) {
+            return 0;
+        } else if (computerSelection === "rock" 
+            && playerSelection === "scissors"
+            || computerSelection === "paper" 
+            && playerSelection === "rock"
+            || computerSelection === "scissors" 
+            && playerSelection === "paper") {
+            //if the computer return -1
+            return -1;
+        } else {
+            //else the player wins return 1
+            return 1;
+        }
+    }
+
+    function game() {
+        for (let i = 0; i < 5; i++) {
+            const win = playARound(computerPlay, choice);
+            if (win == 0) {
+                console.log(win);
+            } else if (win > 0) {
+                playerPoints++;
+                console.log(win);
+            } else {
+                cpuPoints++;
+                console.log(win);
+            }
+        }
+        if (playerPoints > cpuPoints) {
+            console.log("victory");
+        } else {
+            console.log("loss");
+        }
+    }
+
+    game();
+}
+
 
 //if box is clicked
     //id is stored
