992feba1d38c698b299e4b762725f59901ad700c
Giorgio <tran.giorgio@gmail.com>
2022-05-20 21:47:02 -1000

    Added transition when clicking on box

--- javascript.js
+++ javascript.js
@@ -7,11 +7,27 @@ boxes.forEach((box) => {
     });
 });
 
+//make box go back to normal when clicked
+// boxes.forEach((box) => {
+//     // add a click listener for each box
+//     box.addEventListener('click', function() {
+//         this.classList.toggle('half');
+//     });
+// });
+boxes.forEach(box => box.addEventListener('transitionend',removeTransition));
 
+boxes.forEach(box => box.addEventListener('click', decreaseSizeOnClick));
 
+function decreaseSizeOnClick(e) {
+    e.target.classList.add('small');
+}
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('small');
+}
 
 //if box is clicked
     //id is stored
     //computer selects a random choice
     //the choices get compared via a comparator
-
