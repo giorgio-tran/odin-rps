c85e4df01c53ef0b2cd93822111f3f749f2f10dd
Giorgio <tran.giorgio@gmail.com>
2022-05-21 22:09:50 -1000

    Implemented 5 rounds

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 const boxes = document.querySelectorAll('.box');
 /* Game Functions */
+let round = 0;
 let playerPoints = 0;
 let cpuPoints = 0;
 let selection = ["rock","paper","scissors"];
@@ -30,6 +31,7 @@ function removeTransition(e) {
         let str1 = selection[Math.floor(Math.random() * 3)];
 
         const container = document.querySelector('.content-2');
+        const container2 = document.querySelector('.content-3');
 
         console.log(`cpu choice: ${str1}`);
 
@@ -37,20 +39,40 @@ function removeTransition(e) {
         let str2 = e.currentTarget.id;
         console.log(`player choice: ${str2}`);
 
+        //adds to round
         //if tie, return 0
         if (str1 == str2) {
             console.log('tie');
             container.textContent = "IT'S A TIE";
+            container2.textContent = `YOU: ${playerPoints} CPU: ${cpuPoints}`;
         } else if (str1 == "rock" && str2 == "scissors"
             || str1 == "paper" && str2 == "rock"
             || str1 == "scissors" && str2 == "paper") {
-            //if the computer return -1
+            cpuPoints++;
             console.log('computer wins');
             container.textContent = "CPU WINS";
+            container2.textContent = `YOU: ${playerPoints} CPU: ${cpuPoints}`;
         } else {
             //else the player wins return 1
+            playerPoints++;
             console.log('player wins');
             container.textContent = "YOU WON";
+            container2.textContent = `YOU: ${playerPoints} CPU: ${cpuPoints}`;
+        }
+
+        round++;
+
+        if (round == 5) {
+            if (playerPoints > cpuPoints) {
+                container.textContent = "CONGRATS ON THE W!";
+            } else if (playerPoints < cpuPoints) {
+                container.textContent = "BETTER LUCK NEXT TIME!";
+            } else {
+                container.textContent = "TIEEEEEEEEE"
+            }
+            playerPoints = 0;
+            cpuPoints = 0;
+            round = 0;
         }
 
 }
