292a497fac0a1558affc729499842c696d97bba0
Giorgio <tran.giorgio@gmail.com>
2022-05-21 20:47:36 -1000

    Added 'event' parameter to playARound, resolved bug

--- javascript.js
+++ javascript.js
@@ -16,6 +16,8 @@ pChoice = box.currentTarget.id;
 console.log(pChoice);
 }));
 
+boxes.forEach(box => box.addEventListener('click', playARound));
+
 /* Transition functions */
 function decreaseSizeOnClick(e) {
     e.stopPropagation();
@@ -36,4 +38,21 @@ function computerPlay() {
     return selection[Math.floor(Math.random() * 3)];
 }
 
+ //play a round: compare player input with computer choice 
+ function playARound(e) {
+        let str1 = computerPlay();
+        let str2 = e.currentTarget.id;
+        //if tie, return 0
+        if (str1 == str2) {
+            console.log('tie');
+        } else if (str1 == "rock" && str2 == "scissors"
+            || str1 == "paper" && str2 == "rock"
+            || str1 == "scissors" && str2 == "paper") {
+            //if the computer return -1
+            console.log('computer wins');
+        } else {
+            //else the player wins return 1
+            console.log('player wins');
+        }
 
+}
