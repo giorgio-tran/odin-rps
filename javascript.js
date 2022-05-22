1cdf77033d3d714e1d122c7288b6956ea7104594
Giorgio <tran.giorgio@gmail.com>
2022-05-21 19:59:43 -1000

    Added 'click' event listener to obtain name of selected div

--- javascript.js
+++ javascript.js
@@ -1,74 +1,39 @@
 const boxes = document.querySelectorAll('.box');
+/* Game Functions */
+let playerPoints = 0;
+let cpuPoints = 0;
+let selection = ["rock","paper","scissors"];
+let cpuChoice = "";
+let pChoice = "";
 
-boxes.forEach(box => box.addEventListener('click', playGame));
-
+/* Animations */
 boxes.forEach(box => box.addEventListener('transitionend',removeTransition));
-
 boxes.forEach(box => box.addEventListener('click', decreaseSizeOnClick));
 
+//obtains player choice
+boxes.forEach(box => box.addEventListener('click', (box) => {
+pChoice = box.currentTarget.id;
+console.log(pChoice);
+}));
+
+/* Transition functions */
 function decreaseSizeOnClick(e) {
+    e.stopPropagation();
     e.target.classList.add('small');
 }
 
 function removeTransition(e) {
+    e.stopPropagation();
     if (e.propertyName !== 'transform') return;
     e.target.classList.remove('small');
 }
 
-//Play a game 
-function playGame(e) {
-    let cpuPoints = 0;
-    let playerPoints = 0;
-    let selection = ["rock","paper","scissors"];
-    let choice = e.currentTarget.id;
-    
-    function computerPlay() {
-        return selection[Math.floor(Math.random() * 3)];
-    }
-
-    function playARound(computerSelection, playerSelection) {
-        //if tie, return 0
-        if (computerSelection === playerSelection) {
-            return 0;
-        } else if (computerSelection === "rock" 
-            && playerSelection === "scissors"
-            || computerSelection === "paper" 
-            && playerSelection === "rock"
-            || computerSelection === "scissors" 
-            && playerSelection === "paper") {
-            //if the computer return -1
-            return -1;
-        } else {
-            //else the player wins return 1
-            return 1;
-        }
-    }
-
-    function game() {
-        for (let i = 0; i < 5; i++) {
-            const win = playARound(computerPlay, choice);
-            if (win == 0) {
-                console.log(win);
-            } else if (win > 0) {
-                playerPoints++;
-                console.log(win);
-            } else {
-                cpuPoints++;
-                console.log(win);
-            }
-        }
-        if (playerPoints > cpuPoints) {
-            console.log("victory");
-        } else {
-            console.log("loss");
-        }
-    }
-
-    game();
+/* RPS Game */
+//return the player's selection
+function computerPlay() {
+    console.log('fn computer play');
+    //selects random number in the array from 0-2
+    return selection[Math.floor(Math.random() * 3)];
 }
 
 
-//if box is clicked
-    //id is stored
-    //computer selects a random choice
-    //the choices get compared via a comparator
