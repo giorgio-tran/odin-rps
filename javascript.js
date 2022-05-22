e7323edcef3237e335f214d39fdfa2f4efcd1995
Giorgio <tran.giorgio@gmail.com>
2022-05-21 21:41:54 -1000

    Added text that updates based on conditionals from playARound

--- javascript.js
+++ javascript.js
@@ -3,19 +3,12 @@ const boxes = document.querySelectorAll('.box');
 let playerPoints = 0;
 let cpuPoints = 0;
 let selection = ["rock","paper","scissors"];
-let cpuChoice = "";
-let pChoice = "";
 
 /* Animations */
 boxes.forEach(box => box.addEventListener('transitionend',removeTransition));
 boxes.forEach(box => box.addEventListener('click', decreaseSizeOnClick));
 
-//obtains player choice
-boxes.forEach(box => box.addEventListener('click', (box) => {
-pChoice = box.currentTarget.id;
-console.log(pChoice);
-}));
-
+/* Play a Round */
 boxes.forEach(box => box.addEventListener('click', playARound));
 
 /* Transition functions */
@@ -31,28 +24,33 @@ function removeTransition(e) {
 }
 
 /* RPS Game */
-//return the player's selection
-function computerPlay() {
-    console.log('fn computer play');
-    //selects random number in the array from 0-2
-    return selection[Math.floor(Math.random() * 3)];
-}
 
  //play a round: compare player input with computer choice 
  function playARound(e) {
-        let str1 = computerPlay();
+        let str1 = selection[Math.floor(Math.random() * 3)];
+
+        const container = document.querySelector('.content-2');
+
+        console.log(`cpu choice: ${str1}`);
+
+        //specifically targets the main div
         let str2 = e.currentTarget.id;
+        console.log(`player choice: ${str2}`);
+
         //if tie, return 0
         if (str1 == str2) {
             console.log('tie');
+            container.textContent = "IT'S A TIE";
         } else if (str1 == "rock" && str2 == "scissors"
             || str1 == "paper" && str2 == "rock"
             || str1 == "scissors" && str2 == "paper") {
             //if the computer return -1
             console.log('computer wins');
+            container.textContent = "CPU WINS";
         } else {
             //else the player wins return 1
             console.log('player wins');
+            container.textContent = "YOU WON";
         }
 
 }
